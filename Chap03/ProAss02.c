eb35aa779cc59f51cee266f95c0c47a211769774
호준 이 <dlghwns4633@gmail.com>
2025-04-02 18:01:04 +0900

    챕터03

---
+++ Chap03/ProAss02.c
@@ -0,0 +1,30 @@
+/* 파일명: ProAss02.c
+
+ * 내용: Programming Assignment. 가로의 길이와 세로의 길이를 입력받아 직사각형의 넓이와 둘레를 구하는 프로그램을 작성하시오.
+
+ * 작성자: 이호준
+
+ * 날짜: 2025.4. 2
+
+ * 버전: v1.0
+
+ */
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void solve() {
+	int ga, sa;
+	printf("가로 길이는? ");
+	scanf("%d", &ga);
+	printf("세로 길이는? ");
+	scanf("%d", &sa);
+	printf("직사각형의 넓이 : %d\n", ga * sa);
+	printf("직사각형의 둘레 : %d\n", 2 * (ga + sa));
+}
+
+int main() {
+	solve();
+	return 0;
+
+}
